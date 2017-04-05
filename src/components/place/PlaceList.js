7c49a0e269cedf6966bb094f3f9933f9e25480ec
Leonardo Steffen <leosteffen84@gmail.com>
2017-04-05 12:52:20 -0700

    adds names for places and changes the header message

--- src/components/place/PlaceList.js
+++ src/components/place/PlaceList.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { PlaceItem } from './PlaceItem';
 
 export const PlaceList = (props) => {
-  const places = [{name:'a', location:'san francisco'}, {name:'b', location:'san'},{name:'c', location:'francisco'}];
+  const places = [{name:"Mama's", location:'san francisco'}, {name:"Burgers", location:'san'},{name:"Gott's", location:'francisco'}];
   return(
     <div>
-      <ul> "This is just a placeholder for the list"
-          {places.map( place => (<PlaceItem name={place.name} location={place.location}/>))}
+      <ul> Places I want to visit:
+          {places.map( place => (<PlaceItem name={place.name} location={place.location} />))}
       </ul>
     </div>
   )
