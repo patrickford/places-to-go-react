50cb7c2aa9ed9a2782613d83ab000dfdd6f2ae5f
Leonardo Steffen <leosteffen84@gmail.com>
2017-04-05 17:28:06 -0700

    more changes to styles, now to get the place box to display place info separate

--- src/components/place/PlaceItem.js
+++ src/components/place/PlaceItem.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export const PlaceItem = (props) => {
   return(
     <div className='Place-Item'>
-      <span>
+      <span className="place-info">
         <label>City: {props.location} </label>
       </span>
-      <span>
+      <span className="place-info">
         <label>Place: {props.name} </label>
       </span>
     </div>
