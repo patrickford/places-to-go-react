caa3e6c9a2fee2d8870b7b717a69a860def0e36d
Leonardo Steffen <leosteffen84@gmail.com>
2017-04-06 16:45:52 -0700

    removes unnecessary divs from PlaceList component

--- src/components/place/PlaceList.js
+++ src/components/place/PlaceList.js
@@ -4,8 +4,8 @@ import { PlaceItem } from './PlaceItem';
 export const PlaceList = (props) => {
   return(
     <div className="Place-List">
-      <div><h1>Places I want to visit:</h1></div>
-      <div><ul> {props.places.map( place => (<PlaceItem key={place.id} name={place.name} location={place.location} />))} </ul></div>
+      <h1>Places I want to visit:</h1>
+      <ul> {props.places.map( place => (<PlaceItem key={place.id} name={place.name} location={place.location} />))} </ul>
     </div>
   )
 }
