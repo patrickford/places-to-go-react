8480b0a72a08e5c8a3e8b2332d443a40a73673bd
Leonardo Steffen <leosteffen84@gmail.com>
2017-04-07 16:51:20 -0700

    adds method to create place via service

--- src/lib/placesService.js
+++ src/lib/placesService.js
@@ -4,3 +4,14 @@ export const loadPlaces = () => {
   return fetch(baseUrl)
     .then(res => res.json());
 }
+
+export const createPlace = (place) => {
+  return fetch(baseUrl, {
+    metod: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(place)
+  }).then(res => place.json())
+}
